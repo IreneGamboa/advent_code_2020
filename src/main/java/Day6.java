0d44e50016994dee21d63aef4cb28a6c08f2be55
Irene Gamboa-Padilla <irene@schoolmint.net>
2020-12-06 20:20:21 -0600

    Day 6 Event

--- src/main/java/Day6.java
+++ src/main/java/Day6.java
@@ -6,11 +6,19 @@ public class Day6 {
     static void readInput(String fileName){
         ReadFileLineByLine read = new ReadFileLineByLine();
         ArrayList<String> lines = read.reader(fileName);
+        int union = union(lines);
+        int intersection = intersection(lines);
+
+        System.out.println("UNION:" + union);
+        System.out.println("INTERSECTION:" + intersection);
+    }
+
+    static int union(ArrayList<String> data){
         HashSet<Character> yesAnswer = new HashSet<>();
         int total = 0;
 
-        for(int i = 0; i <  lines.size(); i++){
-            String answers = lines.get(i);
+        for(int i = 0; i <  data.size(); i++){
+            String answers = data.get(i);
             if(answers.length() == 0){
                 total = total + yesAnswer.size();
                 yesAnswer = new HashSet<>();
@@ -20,11 +28,43 @@ public class Day6 {
                 }
             }
 
-            if(i == lines.size()-1){
+            if(i == data.size()-1){
                 total = total + yesAnswer.size();
             }
         }
+        return total;
+    }
 
-        System.out.println(total);
+    static int intersection(ArrayList<String> data){
+        ArrayList<HashSet> groupAnswer  = new ArrayList<>();
+        int total = 0;
+
+        for(int i = 0; i <  data.size(); i++){
+            HashSet<Character> answerByPerson = new HashSet<>();
+            String answers = data.get(i);
+            if(answers.length() == 0){
+                HashSet<Character> intersection = groupAnswer.get(0);
+                for(int x = 1; x < groupAnswer.size(); x++){
+                    intersection.retainAll(groupAnswer.get(x));
+                }
+                total = total + intersection.size();
+                groupAnswer = new ArrayList<>();
+            } else {
+                for(int j = 0; j < answers.length(); j++){
+                    answerByPerson.add(answers.charAt(j));
+                }
+                groupAnswer.add(answerByPerson);
+            }
+
+            if(i == data.size()-1){
+                HashSet<Character> intersection = groupAnswer.get(0);
+                for(int x = 1; x < groupAnswer.size(); x++){
+                    intersection.retainAll(groupAnswer.get(x));
+                }
+
+                total = total + intersection.size();
+            }
+        }
+        return total;
     }
 }
